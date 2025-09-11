f1d7adda801cef01a00626ee4b6cf19538ed226b
Manik <beingdeveloper84@gmail.com>
2025-09-12 03:52:39 +0530

    final ig

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCithKgs85DvqjJ8nzj7Wv3WYfHiTALxz0",
-  authDomain: "wecare-cosmic.firebaseapp.com",
-  projectId: "wecare-cosmic",
-  storageBucket: "wecare-cosmic.firebasestorage.app",
-  messagingSenderId: "314582419105",
-  appId: "1:314582419105:web:826bef31624c64800d52e0",
-  measurementId: "G-LS09XKEDB2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
